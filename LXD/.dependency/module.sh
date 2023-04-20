604899830a6d60a8e9af600232afdd7bf8dba6f3
Dark Star☀🎵 <darkstar@unisocial.net>
2023-04-20 03:34:30 -0400

    new dependency manager

--- LXD/.dependency/module.sh
+++ LXD/.dependency/module.sh
@@ -1,18 +1,42 @@
-url="https://github.com/Unicorn-OS/Snapcraft.git"
-package="Snapcraft"
-role="Snapcraft"
+# Environment
+dev=true
 
+# this.source
+git_host="github.com"
+namespace="Unicorn-OS"
+package="Snapcraft"
+module=$package
+ansible_role=$module
+
+clone(){
+	# Either for Development or read-only
+	if [ $dev ]; then
+		url="git@"${git_host}":"${namespace}/${package}".git"
+	else
+		url="https://"${git_host}/${namespace}/${package}".git"
+	fi
+	if [ ! -d "${package}" ]; then 
+		git clone $url
+	fi
+}
 
 link_dependency(){
-  dir=".dependency"
-  ansible_dir="ansible/roles/.dependency"
-  cd ..
-  mkdir $ansible_dir
-  ln -srf $dir/$package/ansible/roles/$role ansible/roles/.dependency/
+	dir=".dependency"
+	ansible_dir="ansible/roles/.dependency"
+	cd ..
+	mkdir -p $ansible_dir
+	ln -srf $dir/$module/ansible/roles/$ansible_role ansible/roles/.dependency/
+	cd -
+}
+
+ignore(){
+	echo "$package" >.gitignore
 }
 
 main(){
-  link_dependency
+	clone
+	link_dependency
+	ignore
 }
 
 main
