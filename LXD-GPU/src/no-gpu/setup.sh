edec97cc5d07b14021ce76dbb532ebaa75a7162b
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-06-11 21:30:22 -0400

    test

--- LXD-GPU/src/no-gpu/setup.sh
+++ LXD-GPU/src/no-gpu/setup.sh
@@ -1,5 +1,7 @@
-lxc profile create x11-no-gpu
-cat x11-no-gpu.profile | lxc profile edit x11-no-gpu
+profile(){
+  lxc profile create x11-no-gpu
+  cat x11-no-gpu.profile | lxc profile edit x11-no-gpu
+}
 
 test(){
   lxc launch ubuntu:22.04 --profile default --profile x11-no-gpu mycontainer
